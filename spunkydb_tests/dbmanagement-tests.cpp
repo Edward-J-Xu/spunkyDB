fcfd0e0d4450697c1efb15acefb6871a481fcf48
Edward-J-Xu <1614257233@qq.com>
2024-06-25 00:30:26 -0400

    add basic testings

---
+++ spunkydb_tests/dbmanagement-tests.cpp
@@ -0,0 +1,51 @@
+#include "catch.hpp"
+#include "tests.h"
+#include "../spunkyDB/spunkydb.h"
+
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+TEST_CASE("Create an empty database", "[createEmptyDB]") {
+    SECTION("Default settings") {
+        std::string dbname = "myemptydb";
+        Database db(SpunkyDB::createEmptyDB(dbname));
+
+        // We know we have been successful when:-
+        // 1. We have a valid database reference returned
+        //   - No test -> The above would have errored
+        // 2. The DB has a folder that exists on the file system
+        REQUIRE(fs::is_directory(fs::status(db.getDirectory())));
+
+        // 3. The database folder is empty (no database files yet)
+        const auto& p = fs::directory_iterator(db.getDirectory());
+        REQUIRE(p == end(p));
+
+        db.destroy();
+        REQUIRE(!fs::exists(fs::status(db.getDirectory())));
+    }
+}
+
+TEST_CASE("Load an existing database", "[loadDB]") {
+    SECTION("Default settings") {
+        std::string dbname = "myemptydb";
+        Database db(SpunkyDB::createEmptyDB(dbname));
+        
+        Database db2(SpunkyDB::loadDB(dbname));
+
+        // We know we have been successful when:-
+        // 1. We have a valid database reference returned
+        //   - No test -> The above would have errored
+        // 2. The DB has a folder that exists on the file system
+        REQUIRE(fs::is_directory(fs::status(db2.getDirectory())));
+
+        // 3. The database folder is empty (no database files yet)
+        const auto& p = fs::directory_iterator(db2.getDirectory());
+        REQUIRE(p == end(p));
+
+        db2.destroy();
+        REQUIRE(!fs::exists(fs::status(db2.getDirectory())));
+    }
+}
+
---
+++ spunkydb_tests/keyvalue-tests.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+#include "../spunkydb/spunkydb.h"
+
+TEST_CASE("Store and retrieve a value", "[setKeyValue, getKeyValue]") {
+    SECTION("Basic Set and Get") {
+        std::string dbname("myemptydb");
+        Database db(SpunkyDB::createEmptyDB(dbname));
+        
+        // We know we have been successful when:-
+        // 1. The retrieved value is the same as the stored value
+        std::string key("simplestring");
+        std::string value("Some highly valuable value");
+        db.setKeyValue(key, value);
+        REQUIRE(value == db.getKeyValue(key));
+
+        db.destroy();
+    }
+}
