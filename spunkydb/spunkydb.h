8334d963cbf5570a71ec3783408428423e93ea2f
Edward-J-Xu <1614257233@qq.com>
2024-06-26 00:25:16 -0400

    standard include pattern

--- spunkydb/spunkydb.h
+++ spunkydb/spunkydb.h
@@ -1,16 +1,1 @@
-#ifndef SPUNKYDB_H
-#define SPUNKYDB_H
-
-#include <string>
-
-#include "database.h"
-
-class SpunkyDB {
-public:
-    SpunkyDB();
-
-    static Database createEmptyDB(std::string& dbname);
-    static Database loadDB(std::string& dbname);
-};
-
-#endif
+#include "include/spunkydb.h"
